3253191bb70326552ef6505f4ea7c482e4bce8f4
bayuswari <bayuswari.01@gmail.com>
2025-06-11 22:27:42 +0700

    feat: implement PasswordHash class and AddUserUseCase with user registration logic

---
+++ src/Applications/security/PasswordHash.ts
@@ -0,0 +1,5 @@
+export class PasswordHash {
+  async hash(password: string): Promise<string> {
+    throw new Error("PASSWORD_HASH.METHOD_NOT_IMPLEMENTED");
+  }
+}
---
+++ src/Applications/use_case/AddUserUseCase.ts
@@ -0,0 +1,23 @@
+import { RegisterUser, type RegisterUserPayload } from "../../Domains/users/entities/RegisterUser";
+import type { UserRepository } from "../../Domains/users/UserRepository";
+import type { PasswordHash } from "../security/PasswordHash";
+
+export class AddUserUseCase {
+  public _userRepository;
+  public _passwordHash;
+
+  constructor({ userRepository, passwordHash }: { userRepository: UserRepository; passwordHash: PasswordHash }) {
+    this._userRepository = userRepository;
+    this._passwordHash = passwordHash;
+  }
+
+  async execute(useCasePayload: RegisterUserPayload) {
+    const registerUser = new RegisterUser(useCasePayload);
+
+    await this._userRepository.verifyAvailableUsername(registerUser.username);
+
+    registerUser.password = await this._passwordHash.hash(registerUser.password);
+
+    return this._userRepository.addUser(registerUser);
+  }
+}
