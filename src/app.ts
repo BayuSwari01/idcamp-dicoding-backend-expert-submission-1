92178c47c69ac853f1cdbacbf6b8c041cde1cd12
bayuswari <bayuswari.01@gmail.com>
2025-06-13 15:17:11 +0700

    feat: initialize server setup with dotenv configuration and server creation logic

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { container } from "./Infrastructures/container";
+import { createServer } from "./Infrastructures/http/createServer";
+
+const start = async () => {
+  const server = await createServer(container);
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
+};
+
+start();
