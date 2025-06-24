75fe618c8347b8551b96415f4670dc73ccc11896
bayuswari <bayuswari.01@gmail.com>
2025-06-24 22:04:04 +0700

    feat: implement CommentRepository class and associated test cases

---
+++ src/Domains/comments/CommentRepository.ts
@@ -0,0 +1,21 @@
+export class CommentRepository {
+  async addComment(payload: any): Promise<void> {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+
+  async verifyCommentIsExist(id: string): Promise<void> {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+
+  async verifyCommentOwner(id: string, owner: string): Promise<void> {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+
+  async deleteComment(id: string): Promise<void> {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+
+  async getCommentsThread(threadId: string): Promise<any[]> {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+  }
+}
